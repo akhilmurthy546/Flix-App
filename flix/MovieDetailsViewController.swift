79bce00e4aa0d929e358dd0a9f601ce36b6f3cf2
Akhil Murthy <akhil.s.murthy@gmail.com>
2020-10-27 21:08:41 -0700

    Flix Part 2 Added

---
+++ flix/MovieDetailsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MovieDetailsViewController.swift
+//  flix
+//
+//  Created by Akhil Murthy on 10/26/20.
+//  Copyright © 2020 Akhil Murthy. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "http://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)!
+        
+        let backdropBaseURL = "http://image.tmdb.org/t/p/w780"
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: backdropBaseURL + backdropPath)!
+        
+        posterView.af.setImage(withURL: posterURL)
+        backdropView.af.setImage(withURL: backdropURL)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Akhil Murthy on 10/26/20.
+//  Copyright © 2020 Akhil Murthy. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
